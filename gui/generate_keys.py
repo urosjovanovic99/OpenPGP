fe2c190a163566844dcd364fae94716faf146394
tozica <64868333+tozica@users.noreply.github.com>
2023-05-14 17:58:26 +0200

    Refactor form.

--- gui/generate_keys.py
+++ gui/generate_keys.py
@@ -1,5 +1,17 @@
+import datetime
+
 from kivy.uix.screenmanager import Screen
+
+from key_rings.private_key_ring import privateKeyRing, PrivateKeyRing
+from rsa_util.rsa_util import generate_keys
 
 
 class GenerateKeysScreen(Screen):
-    pass
+    def collect_data(self, name, email, key_size, algorithm):
+        if algorithm == 'RSA':
+            (publicKey, privateKey) = generate_keys(int(key_size))
+
+
+
+
+
