cbbb9d377bb6fcf87998161722083dffecc5b9d0
tozica <64868333+tozica@users.noreply.github.com>
2023-05-28 16:49:23 +0200

    Resolve conflicts.

--- application.py
+++ application.py
@@ -1,13 +1,12 @@
-import os
-from os.path import dirname
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
-from gui.delete_keys import DeleteKeysScreen
-from gui.generate_keys import GenerateKeysScreen
-from gui.openPGP import OpenPGP
+from gui.delete_keys.delete_keys import DeleteKeysScreen
+
+from gui.generate_keys.generate_keys import GenerateKeysScreen
+from gui.import_key.import_key import ImportKeyScreen
+from gui.main_screen.open_pgp import OpenPGP
 
 
 class OpenPGPApp(App):
@@ -19,6 +18,7 @@ def load_gui():
     Builder.load_file('gui/generate_keys/generate_keys.kv')
     Builder.load_file('gui/import_key/import_key.kv')
     Builder.load_file('gui/main_screen/open_pgp.kv')
+    Builder.load_file('gui/delete_keys/delete_keys.kv')
 
 
 if __name__ == '__main__':
@@ -27,4 +27,5 @@ if __name__ == '__main__':
     screen_manager.add_widget(OpenPGP(name="open_pgp_screen"))
     screen_manager.add_widget(GenerateKeysScreen(name="generate_keys_screen"))
     screen_manager.add_widget(ImportKeyScreen(name="import_key_screen"))
+    screen_manager.add_widget(DeleteKeysScreen(name="delete_keys"))
     OpenPGPApp().run()
